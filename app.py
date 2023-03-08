e6226ec5d6fe840f1ca9b1b7815a00dca8ea03bc
ccastri <camilo.castrillon@uao.edu.co>
2023-03-07 22:45:24 -0500

    backend Services for server side rendering now and on...

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/get', methods=['GET'])
+def get_LitisPackages():
+    return jsonify([{'litis': 'decolombia '}])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
