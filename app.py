4471178d3116cd9657ce6562c28f1c9f35f0bfbe
ccastri <camilo.castrillon@uao.edu.co>
2023-03-08 00:09:40 -0500

    Database has been connected. 1st table got created

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+# import init_app
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/litis'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+db = SQLAlchemy(app)
+
+
+class Articles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    body = db.Column(db.Text())
+    date = db.Column(db.DateTime, default=datetime.datetime.now)
+
+
+with app.app_context():
+    db.create_all()
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
 
 
 @app.route('/get', methods=['GET'])
