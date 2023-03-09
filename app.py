2ea614ed87359c96d6bd88a01e6c160550f87ab9
ccastri <camilo.castrillon@uao.edu.co>
2023-03-09 09:14:28 -0500

    Crud methods done

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import datetime
-# import init_app
+from flask_marshmallow import Marshmallow
 
 
 app = Flask(__name__)
@@ -9,6 +9,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/litis'
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+with app.app_context():
+    db.create_all()
 
 
 class Articles(db.Model):
@@ -17,18 +21,69 @@ class Articles(db.Model):
     body = db.Column(db.Text())
     date = db.Column(db.DateTime, default=datetime.datetime.now)
 
-
-with app.app_context():
-    db.create_all()
-
     def __init__(self, title, body):
         self.title = title
         self.body = body
 
 
+# Serializing data to submit it to the frontend
+
+
+class ArticleSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'body', 'date')
+
+
+# For one article
+article_schema = ArticleSchema()
+# For a set of articles
+articles_schema = ArticleSchema(many=True)
+
+
 @app.route('/get', methods=['GET'])
-def get_LitisPackages():
-    return jsonify([{'litis': 'decolombia '}])
+def get_allLitisPackages():
+    all_articles = Articles.query.all()
+    results = articles_schema.dump(all_articles)
+    return jsonify(results)
+
+
+@app.route('/get/<id>/', methods=['GET'])
+def get_LitisPackagesById(id):
+    article = Articles.query.get(id)
+    return article_schema.jsonify(article)
+
+
+@app.route('/update/<id>/', methods=['PUT'])
+def update_LitisPackagesById(id):
+    article = Articles.query.get(id)
+
+    title = request.json['title']
+    body = request.json['body']
+
+    article.title = title
+    article.body = body
+
+    db.session.commit()
+    return article_schema.jsonify(article)
+
+
+@app.route('/add', methods=['POST'])
+def add_article():
+    title = request.json['title']
+    body = request.json['body']
+    articles = Articles(title, body)
+    db.session.add(articles)
+    db.session.commit()
+    return article_schema.jsonify(articles)
+
+
+@app.route('/delete/<id>/', methods=['DELETE'])
+def delete_LitisPackagesById(id):
+    article = Articles.query.get(id)
+    db.session.delete(article)
+    db.session.commit()
+
+    return article_schema.jsonify(article)
 
 
 if __name__ == "__main__":
