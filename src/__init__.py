7a091242c49d2090491ad41267f878c4bd84e553
ccastri <camilo.castrillon@uao.edu.co>
2023-03-12 18:03:52 -0500

    now src is serving the app

---
+++ src/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config is None:
+
+        app.config.from_mapping(
+            SECRET_KEY="dev"
+        )
+
+    #!Test config defined
+    else:
+        app.config.from_mapping(test_config)
+
+    @app.route('/get2', methods=['GET'])
+    def get_allLitisPackages():
+        return {"hello": "world"}
+
+    return app
