2de2f0f3344888e1106e6c549e5e6389b3f539d1
ccastri <camilo.castrillon@uao.edu.co>
2023-03-30 20:11:40 -0500

    Messing up with marshmallow

--- src/auth.py
+++ src/auth.py
@@ -3,214 +3,108 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
 # import validators
 from flask_jwt_extended import jwt_required, create_access_token, create_refresh_token, get_jwt_identity
-from src.database import User, db
-# from src import RegisterForm
+from src.database import User, db, user_schema, users_schema
+# Serialization and data validation
+from flask_marshmallow import Marshmallow
+from marshmallow import fields, Schema
+
+from src import app
 
 auth = Blueprint("auth", __name__, url_prefix="/api/v1/auth")
+ma = Marshmallow(app)
+#! user form with WTForms DEFAULT validators:
 
 
-class RegisterForm(Form):
-
-    first_name = StringField('First Name', [
-        validators.DataRequired(),
-        validators.Length(min=2, max=25)])
-    last_name = StringField('Last Name', [
-        validators.DataRequired(),
-        validators.Length(min=2, max=25)])
-    phone_number = StringField(
-        'Phone Number', [
-            validators.DataRequired(),
-            validators.Length(min=10, max=10)])
-    username = StringField('Username', [
-        validators.DataRequired(),
-        validators.Length(min=2, max=25)])
-    email = StringField('Email', [
-        validators.DataRequired(),
-        validators.Length(min=2, max=25)])
-    #!Para verificar que el email inicie con google
-    # User.query.filter(User.email.endswith('@gmail.com')).all()
-    # return self.isGoogle
-
-    password = PasswordField('Password', [
-        validators.DataRequired(),
-        validators.Length(min=2, max=25),
-        validators.EqualTo('confirm_password',
-                           message="Confirm Password didn't match")
-    ])
-    confirm_password = PasswordField('Confirm Password', [
-        validators.DataRequired(),
-        validators.Length(min=2, max=25)])
-    neighborhood = StringField(
-        'neighborhood', [
-            validators.DataRequired(),
-            validators.Length(min=2, max=25)])
-    city = StringField('city', [
-        validators.DataRequired(),
-        validators.Length(min=2, max=25)])
-    department = StringField('department', [
-        validators.DataRequired(),
-        validators.Length(min=2, max=25)])
-    img = StringField('img', [
-        validators.DataRequired(),
-        validators.Length(min=2, max=25)])
-    accept_tos = BooleanField(
-        'I accept the <a href="/tos/">Terms of service </a> and the <a href="/privacy/> Privacy Notice </a> (Last updated jul 16 2016)',
-        [validators.DataRequired(), ])
-
-
-@auth.post('/register')
+@auth.route('/register', methods=['POST'])
 def register():
 
-    first_name = request.json['first_name']
-    last_name = request.json['last_name']
-    phone_number = request.json['phone_number']
-    username = request.json['username']
-    email = request.json['email']
-    password = request.json['password']
-    # password2 = request.json['password2']
-    confirm_password = request.json['password']
-    neighborhood = request.json['neighborhood']
-    city = request.json['city']
-    department = request.json['department']
-    img = request.json['img']
-    # tos_is_clicked = request.json['tos_is_clicked']
-    # plan_id = request.json['plan']
-    # created_at = request.json['created_at']
+    #! Acceso a las columnas de la BB.DD. en la tabla usuario
+    # first_name = request.get_json['firstName']
+    # last_name = request.get_json['lastName']
+    # phone_number = request.get_json['phoneNumber']
+    # username = request.get_json['username']
+    # email = request.get_json['email']
+    # password = request.get_json['password']
+    # # password2 = request.get_json['password2']
+    # confirm_password = request.get_json['confirmPassword']
+    # neighborhood = request.get_json['neighborhood']
+    # city = request.get_json['city']
+    # department = request.get_json['department']
+    # img = request.get_json['img']
+    # tos_is_clicked = request.get_json['tos_is_clicked']
+    # # plan_id = request.json['plan']
+    # created_at = request.get_json['created_at']
+    form_data = request.get_json()
+    # errors = user_schema.validate(form_data)
 
-    # form = RegisterForm(request.form)
+    # if not username.isalnum() or " " in username:
+    #     return jsonify({
+    #         "error": 'Nombre de usuario debe contener numeros y letras, sin espacios spaces'
+    #     })
+    # #! Email:
+    # if not validators.email(email):
+    #     return jsonify({
+    #         'error': "Email is not valid"
+    #     })
+    # # !Email y usuario unicos en la BB.DD.:
+    # if User.query.filter_by(email=email).first() is not None:
+    #     return jsonify({"error": "Email is taken already"})
+    # if User.query.filter_by(username=email).first() is not None:
+    #     return jsonify({"error": "username is taken already"})
 
-#! Validaciones:
+    # #!Hasheo de la contraseña
+    # password_hash = generate_password_hash(password)
+    # #! Nueva instancia de la clase usuario
+    # user = User(first_name=first_name, last_name=last_name, phone_number=phone_number, confirm_password=password_hash,
+    # username=username, password=password_hash, email=email, neighborhood=neighborhood, city=city, department=department, img=img, tos_is_clicked=tos_is_clicked)
 
-    #! first_name:
-    if " " in first_name:
-        return jsonify({
-            "error": "Por favor ingrese su nombre"
-        })
-    if len(last_name) < 2:
-        return jsonify({
-            "error": 'Nombre invalido'
-        })
-    #! Apellidos:
-    if " " in last_name:
-        return jsonify({
-            "error": "Por favor ingrese su apellido"
-        })
-    if len(last_name) < 2:
-        return jsonify({
-            "error": 'Apellido invalido'
-        })
-    #! Numero de telefono:
-    if " " in phone_number:
-        return jsonify({
-            "error": "Por favor ingrese un numero de telefono"
-        })
-    if len(phone_number) != 10:
-        return jsonify({
-            "error": "Por favor ingrese un numero de telefono valido"
-        })
-    if len(phone_number) != 10:
-        return jsonify({
-            "error": "Por favor ingrese un numero de telefono valido"
-        })
-    if User.query.filter_by(phone_number=phone_number).first() is not None:
-        return jsonify({"error": "Phone number is taken already"})
-    #! neighborhood:
-    if " " in neighborhood:
-        return jsonify({
-            "error": "Por favor ingrese el barrio (sector) de residencia"
-        })
-    #!Ciudad:
-    if " " in city:
-        return jsonify({
-            "error": "Por favor seleccione su departamento de residencia"
-        })
-    #! Departamento:
-    if " " in department:
-        return jsonify({
-            "error": "Por favor seleccione su departamento de residencia"
-        })
-    #!Password:
-    if len(password) < 6:
-        return jsonify({
-            'Password too short'
-        })
-    # TODO: REGEX para el password
+    # print(user)
+    # db.session.add(user)
+    # db.session.commit()
 
-    #!Username:
-    if len(username) < 3:
-        return jsonify({
-            "error": 'username is too short'
-        })
-    if not username.isalnum() or " " in username:
-        return jsonify({
-            "error": 'Nombre de usuario debe contener numeros y letras, sin espacios spaces'
-        })
-    #! Email:
-    if not validators.email(email):
-        return jsonify({
-            'error': "Email is not valid"
-        })
-    #!Email y usuario unicos en la BB.DD.:
-    if User.query.filter_by(email=email).first() is not None:
-        return jsonify({"error": "Email is taken already"})
-    if User.query.filter_by(username=email).first() is not None:
-        return jsonify({"error": "username is taken already"})
+    # return user_schema.jsonify(user)
+    return jsonify({'this is a user'})
 
-    #!Hasheo de la contraseña
-    password_hash = generate_password_hash(password)
-    #! Nueva instancia de la clase usuario
-    user = User(first_name=first_name, last_name=last_name, phone_number=phone_number, confirm_password=password_hash,
-                username=username, password=password_hash, email=email, neighborhood=neighborhood, city=city, department=department, img=img,)
-    #!Añadir a la base de datos
-    db.session.add(user)
-    db.session.commit()
-# TODO: AÑADIR render_template('profile.html') from react!!!
-    return render_template('register.html')
-    # return jsonify({
-    #     'message': "User created. Status code: 200",
-    #     'user': {
-    #         'first_name': first_name,
-    #         'last_name': last_name,
-    #         'phone_number': phone_number,
-    #         'username': username,
-    #         'email': email,
-    #         'password': password_hash,
-    #         'neighborhood': neighborhood,
-    #         'city': city,
-    #         'department': department,
-    #     }
-    # })
+    # if request.method == 'GET':
+    #     # return 'this is the register view'
+    #     return 'thats the view'
 
 
-@auth.post('/login')
+@auth.route('/login', methods={'GET': 'POST'})
 def login():
-    email = request.json.get('email', '')
-    password = request.json.get('password', '')
+    if request.method == 'GET':
+        return 've el login ve'
+    if request.method == 'POST':
+        email = request.json.get('email', '')
+        password = request.json.get('password', '')
 
-    # print(email, password)
+        # print(email, password)
+        # try:
+        # if (email)
+        user = User.query.filter_by(email=email).first()
+        print(user.password)
+        if user:
+            is_pass_correct = check_password_hash(user.password, password)
 
-    user = User.query.filter_by(email=email).first()
-    print(user.password)
-    if user:
-        is_pass_correct = check_password_hash(user.password, password)
+            # print(is_pass_correct)
+            if is_pass_correct:
+                refresh = create_refresh_token(identity=user.id)
+                access = create_access_token(identity=user.id)
 
-        # print(is_pass_correct)
-        if is_pass_correct:
-            refresh = create_refresh_token(identity=user.id)
-            access = create_access_token(identity=user.id)
+                return jsonify({
+                    'user': {
+                        'refresh': refresh,
+                        'access': access,
+                        'username': user.username,
+                        'email': email
+                    }
+                })
+        # except:
 
-            return jsonify({
-                'user': {
-                    'refresh': refresh,
-                    'access': access,
-                    'username': user.username,
-                    'email': email
-                }
-            })
-    return jsonify({'error': 'wrong credentials'})
+        return jsonify({'error': 'wrong credentials'})
 
 
-@auth.get('/me')
+@auth.get('/dashborard/profile/id')
 @jwt_required()
 def auth_route():
     #! Para desplegar la informacion del perfil y completar con la seccion de docs
@@ -230,3 +124,66 @@ def refresh_users_token():
     access = create_access_token(identity=identity)
 
     return jsonify({'access': access})
+
+    #! Validaciones:
+
+    # #! first_name:
+    # if " " in first_name:
+    #     return jsonify({
+    #         "error": "Por favor ingrese su nombre"
+    #     })
+    # if len(last_name) < 2:
+    #     return jsonify({
+    #         "error": 'Nombre invalido'
+    #     })
+    # #! Apellidos:
+    # if " " in last_name:
+    #     return jsonify({
+    #         "error": "Por favor ingrese su apellido"
+    #     })
+    # if len(last_name) < 2:
+    #     return jsonify({
+    #         "error": 'Apellido invalido'
+    #     })
+    # #! Numero de telefono:
+    # if " " in phone_number:
+    #     return jsonify({
+    #         "error": "Por favor ingrese un numero de telefono"
+    #     })
+    # if len(phone_number) != 10:
+    #     return jsonify({
+    #         "error": "Por favor ingrese un numero de telefono valido"
+    #     })
+    # if len(phone_number) != 10:
+    #     return jsonify({
+    #         "error": "Por favor ingrese un numero de telefono valido"
+    #     })
+    # if User.query.filter_by(phone_number=phone_number).first() is not None:
+    #     return jsonify({"error": "Phone number is taken already"})
+    # #! neighborhood:
+    # if " " in neighborhood:
+    #     return jsonify({
+    #         "error": "Por favor ingrese el barrio (sector) de residencia"
+    #     })
+    # #!Ciudad:
+    # if " " in city:
+    #     return jsonify({
+    #         "error": "Por favor seleccione su departamento de residencia"
+    #     })
+    # #! Departamento:
+    # if " " in department:
+    #     return jsonify({
+    #         "error": "Por favor seleccione su departamento de residencia"
+    #     })
+    # #!Password:
+    # if len(password) < 6:
+    #     return jsonify({
+    #         'Password too short'
+    #     })
+    # # TODO: REGEX para el password
+
+    # #!Username:
+    # if len(username) < 3:
+    #     return jsonify({
+    #         "error": 'username is too short'
+    #     })
--- src/database.py
+++ src/database.py
@@ -25,7 +25,7 @@ class Plan(db.Model):
     img = db.Column(db.String(100), unique=True, nullable=False)
     #!Especifico la tabla usuarios y su columna ID para la foreign key
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    created_at = db.Column(db.DateTime, default=datetime.datetime.now)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
     #!Relacion con la tabla de facturas (plan: padre. Factura:children)
     # bill = db.relationship('Bill', backref="plan")
 
@@ -54,25 +54,29 @@ class User(db.Model):
     confirm_password = db.Column(db.String(50), nullable=True)
     neighborhood = db.Column(db.String(20), nullable=False)
     city = db.Column(db.String(30), nullable=False)
-    department = db.Column(db.Text(), nullable=False)
+    department = db.Column(db.String(30), nullable=False)
     img = db.Column(db.String(50), nullable=False)
+    tos_is_clicked = db.Column(db.Boolean(), default=False)
     created_at = db.Column(db.DateTime, default=datetime.datetime.now)
-    updated_at = db.Column(db.DateTime, onupdate=datetime.datetime.now)
+    # updated_at = db.Column(db.DateTime, onupdate=datetime.datetime.now)
     plan = db.relationship('Plan', backref="user")
     # bill = db.relationship('Bill', backref="user")
 
-    def __init__(self, id: int, first_name: str, last_name: str, phone_number: str, email: str, username: str, password: str, neighborhood: str, city: str, department: str, img: str):
-        self.id = 1
+    def __init__(self, id: int, first_name: str, last_name: str, phone_number: str, email: str, username: str, password: str, confirm_password: str, neighborhood: str, city: str, department: str, img: str, tos_is_clicked: bool, created_at: str):
+        self.id = id
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
         self.email = email
         self.username = username
         self.password = password
+        self.confirm_password = confirm_password
         self.neighborhood = neighborhood
         self.city = city
         self.department = department
         self.img = img
+        self.created_at = created_at
+        self.tos_is_clicked = tos_is_clicked
 
     def __repr__(self) -> str:
         return f'User>>> {self.username}'
@@ -80,6 +84,16 @@ class User(db.Model):
 #! Pending for testing
 
 
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'first_name', 'last_name', 'phone_number', 'email', 'username',
+                  'password', 'confirm_password', 'neighborhood', 'city', 'department', 'img', 'tos_is_clicked', 'created_at')
+
+
+#! For one article
+user_schema = UserSchema()
+#! For a set of articles
+users_schema = UserSchema(many=True)
 # class Bill(db.Model):
 
 #     __tablename__ = 'bill'
